0a8172369e5d21a58f17cb05cb6d3b0a15c70b75
Anuj Sharma <anujsharmajan06@gmail.com>
2026-01-11 23:58:30 +0530

    Binary Search on reverse sorted array (descending order)

---
+++ src/arrays/patterns/binarysearch/ReverseBinarySearch.java
@@ -0,0 +1,32 @@
+package arrays.patterns.binarysearch;
+
+public class ReverseBinarySearch {
+    /**
+     * Problem: Reverse Sorted Binary Search (Descending Order)
+     * Time Complexity: O(log n)
+     * Space Complexity: O(1)
+     */
+    static int reverseBinarySearch(int[] arr,int target){
+        // Edge case: null or empty array
+        if(arr == null || arr.length < 1) return -1;
+
+        int start = 0;
+        int end = arr.length-1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(arr[mid] == target){
+                return mid;
+            } else if (target < arr[mid]) {
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {20, 17, 15, 14, 13, 10, 8, 5, 3};
+        int target = 14;
+        System.out.println(reverseBinarySearch(arr,target));
+    }
+}
