419e7061e1d6648aa661c8373d714de38b4ab7a3
Anuj Sharma <anujsharmajan06@gmail.com>
2026-07-20 15:09:24 +0530

    solve: balanced parentheses checker using stack

---
+++ src/capgeminipractice/BalancedParenthesesChecker.java
@@ -0,0 +1,36 @@
+package capgeminipractice;
+
+import java.util.Stack;
+
+public class BalancedParenthesesChecker {
+    static boolean isBalanced(String str) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            }else {
+                if(ch == ')'){
+                    if(stack.isEmpty() || stack.pop() != '('){
+                        return false;
+                    }
+                }
+                if(ch == '}'){
+                    if(stack.isEmpty() || stack.pop() != '{'){
+                        return false;
+                    }
+                }
+                if(ch == ']'){
+                    if(stack.isEmpty() || stack.pop() != '['){
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        String str = "{[(])}";
+        System.out.println(isBalanced(str));
+    }
+}
