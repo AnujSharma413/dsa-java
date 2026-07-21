60d434c6f09bc6ae3c20bdf1af9d13fc84ee3558
Anuj Sharma <anujsharmajan06@gmail.com>
2026-07-21 18:54:45 +0530

    solve: rotate array by k positions in-place using reversal technique

---
+++ src/capgeminipractice/RotateAnArrayByKPositions.java
@@ -0,0 +1,28 @@
+package capgeminipractice;
+
+import java.util.Arrays;
+
+public class RotateAnArrayByKPositions {
+    static void rotateArray(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n;
+        reverse(arr,0,n-1);
+        reverse(arr,0,k-1);
+        reverse(arr,k,n-1);
+    }
+    static void reverse(int[] arr, int left, int right){
+        while(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        rotateArray(arr,k);
+        System.out.println(Arrays.toString(arr));
+    }
+}
