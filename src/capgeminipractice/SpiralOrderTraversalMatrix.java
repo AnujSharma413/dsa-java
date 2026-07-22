e32378b463b347dca497c2823c7e53daaefe37c5
Anuj Sharma <anujsharmajan06@gmail.com>
2026-07-22 16:50:16 +0530

    solve: spiral order traversal using four shrinking boundaries

---
+++ src/capgeminipractice/SpiralOrderTraversalMatrix.java
@@ -0,0 +1,50 @@
+package capgeminipractice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralOrderTraversalMatrix {
+    static List<Integer> spiralOrder(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int left = 0;
+        int right = m - 1;
+        int top = 0;
+        int bottom = n - 1;
+        List<Integer> ans = new ArrayList<>();
+
+        while(top <= bottom && left <= right){
+            //right
+            for(int i = left; i <= right; i++){
+                ans.add(matrix[top][i]);
+            }
+            top++;
+            //bottom
+            for(int i = top; i <= bottom; i++){
+                ans.add(matrix[i][right]);
+            }
+            right--;
+            //left
+            if(top <= bottom){
+                for(int i = right; i >= left; i--){
+                    ans.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            //top
+            if(left <= right){
+                for(int i = bottom; i >= top; i--){
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},
+                          {4,5,6},
+                          {7,8,9}};
+        System.out.println(spiralOrder(matrix));
+    }
+}
