026da93c822c9d14292e0314dea72eb2bc42531f
Anuj Sharma <anujsharmajan06@gmail.com>
2026-07-23 23:12:43 +0530

    solve: check if a number is prime using sqrt(n) optimization

---
+++ src/capgeminipractice/CheckNumberIsPrime.java
@@ -0,0 +1,20 @@
+package capgeminipractice;
+
+public class CheckNumberIsPrime {
+    static boolean isPrime(int n) {
+        if(n <= 1){
+            return false;
+        }
+        for(int i = 2; i * i <= n; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int num = 29;
+        System.out.println(isPrime(num));
+    }
+}
