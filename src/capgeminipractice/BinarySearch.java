5760d6ce2aef88a537140372a11d3685d04b68ae
Anuj Sharma <anujsharmajan06@gmail.com>
2026-07-23 23:12:57 +0530

    solve: binary search implementation, fixed off-by-one boundary bug

---
+++ src/capgeminipractice/BinarySearch.java
@@ -0,0 +1,24 @@
+package capgeminipractice;
+
+public class BinarySearch {
+    static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length-1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(arr[mid] == target){
+                return mid;
+            }else if(arr[mid] < target){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+        int target = 23;
+        System.out.println(binarySearch(arr,target));
+    }
+}
