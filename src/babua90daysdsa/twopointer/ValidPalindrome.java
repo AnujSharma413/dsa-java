6707c4188155132e6ac3f4de6de5504a6072318d
Anuj Sharma <anujsharmajan06@gmail.com>
2026-08-23 22:46:00 +0530

    feat(twopointer): add Valid Palindrome problem solution

---
+++ src/babua90daysdsa/twopointer/ValidPalindrome.java
@@ -0,0 +1,32 @@
+package babua90daysdsa.twopointer;
+
+public class ValidPalindrome {
+    static boolean isPalindrome(String s){
+        int left = 0;
+        int right = s.length()-1;
+        while(left < right){
+            while(left < right && !isAlphaNumeric(s.charAt(left))){
+                left++;
+            }
+            while(left < right && !isAlphaNumeric(s.charAt(right))){
+                right--;
+            }
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(str));
+    }
+    static boolean isAlphaNumeric(char ch){
+        return (ch >= 'a' && ch <= 'z') ||
+                (ch >= 'A' && ch <= 'Z') ||
+                (ch >= '0' && ch <= '9');
+    }
+}
