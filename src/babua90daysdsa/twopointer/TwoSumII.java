5544fc4eb8d94d92dd3e31f138637f3ef2ba2a24
Anuj Sharma <anujsharmajan06@gmail.com>
2026-09-01 19:22:01 +0530

    feat(twopointer): add O(n) two-pointer solution for TwoSumII

---
+++ src/babua90daysdsa/twopointer/TwoSumII.java
@@ -0,0 +1,26 @@
+package babua90daysdsa.twopointer;
+
+import java.util.Arrays;
+
+public class TwoSumII {
+    static int[] twoSum(int[] numbers, int target){
+        int i = 0;
+        int j = numbers.length-1;
+        while(i < j){
+            if(numbers[i] + numbers[j] == target){
+                return new int[]{i+1,j+1};
+            }else if(numbers[i] + numbers[j] < target){
+                i++;
+            }else{
+                j--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,7,11,15};
+        int target = 9;
+        int[] res = twoSum(arr,target);
+        System.out.println(Arrays.toString(res));
+    }
+}
