d1909cc5d5091ec178e5f4a7c71ca03be7528cb5
Anuj Sharma <anujsharmajan06@gmail.com>
2026-09-17 19:34:58 +0530

    refactor(fastslow): extract ListNode into standalone class and clean MiddleOfTheLinkedList

--- src/babua90daysdsa/fastslow/MiddleOfTheLinkedList.java
+++ src/babua90daysdsa/fastslow/MiddleOfTheLinkedList.java
@@ -1,13 +1,5 @@
 package babua90daysdsa.fastslow;
 
-class ListNode {
-     int val;
-     ListNode next;
-     ListNode() {}
-     ListNode(int val) { this.val = val; }
-     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- }
-
 public class MiddleOfTheLinkedList {
     public ListNode middleNode(ListNode head) {
 
